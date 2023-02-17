c0fc781b295923c0f003a3a60590bb7c23657409
Kobyokl <youmanskobe@gmail.com>
2023-02-16 21:05:53 -0500

    First commit

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+// button script
+$("#opbutton").mouseover(function() {
+    var audio = new Audio('js/first_select.mp3');
+    audio.volume = 0.05;
+    audio.play();
+})
+
+
+//video collage and text change scripts
